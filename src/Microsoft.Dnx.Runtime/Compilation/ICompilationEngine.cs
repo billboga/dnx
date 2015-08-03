160f8671985eecc611f65aca6f518237f0585f1e
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2015-08-03 12:14:10 -0700

    LibraryExport refactoring, phase 2

---
+++ src/Microsoft.Dnx.Runtime/Compilation/ICompilationEngine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.Versioning;
+
+namespace Microsoft.Dnx.Runtime.Compilation
+{
+    public interface ICompilationEngine : IDisposable
+    {
+        event Action<string> OnInputFileChanged;
+
+        Assembly LoadProject(Project project, FrameworkName targetFramework, string configuration, string aspect, IAssemblyLoadContext loadContext);
+    }
+}
